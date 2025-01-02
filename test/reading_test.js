003dfccc3a0b0b838c26a8e55062d2e2a03bec55
Pierre Lasante <pierre.lasante@videotron.ca>
2025-01-02 08:05:46 -0500

    how to findOne

--- test/reading_test.js
+++ test/reading_test.js
@@ -3,22 +3,33 @@ const Student = require('../src/student');
 
 describe('Read student table', () => {
   let student;
+  let student2;
 
   beforeEach((done) => {
     student = new Student({ name: 'tarzan'});
+    student2 = new Student({ name: 'tarzan2' });
     student.save()
+    student2.save()
       .then(() => done());
   });
 
-  it('Find all the students', (done) => {
-    Student.find({ name: 'tarzan' })
-      .then((students) => {
-        if (!students.length) {
-          return done(new Error('No students found'));
-        }
-        assert(String(student._id) === String(students[0]._id));
+  // it('Find all the students', (done) => {
+  //   Student.find({ name: 'tarzan' })
+  //     .then((students) => {
+  //       if (!students.length) {
+  //         return done(new Error('No students found'));
+  //       }
+  //       assert(String(student._id) === String(students[0]._id));
+  //       done();
+  //     })
+  //     .catch(done); // Note: here, if there is an error, done() should be called with the error
+  // });
+  it('Find one of the student', (done) => {
+    Student.findOne({_id: student2._id})
+      .then(student => {
+        assert(student.name === 'tarzan2');
         done();
       })
-      .catch(done); // Note: here, if there is an error, done() should be called with the error
-  });
+      .catch(done);
+  })
 });

697254cc753bb9aebc3415492b9158a4bd7881aa
Pierre Lasante <pierre.lasante@videotron.ca>
2025-01-03 11:30:24 -0500

    testing a subdocument_test.js avec callbacks et async/await 2

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -22,20 +22,54 @@ describe('Subdocument sans callback', () => {
 describe('Subdocument avec callback', () => {
   it('Creates a subdocument', (done) => {
     const student = new Student({
-      name: 'Pierre',
+      name: 'Karo',
       articles : [{title: 'JavaScript'}]
     });
 
     student.save()
       .then(() => {
-        Student.findOne({ name: 'Pierre' })
+        Student.findOne({ name: 'Karo' })
           .then((savedStudent) => {
-            assert(savedStudent.name === 'Pierre');
+            assert(savedStudent.name === 'Karo');
             assert(savedStudent.articles[0].title === 'JavaScript');
             done();
           })
           .catch(done); // Si une erreur se produit lors de la recherche, cela terminera le test avec une erreur
       })
       .catch(done); // Si une erreur se produit lors de l'enregistrement, cela terminera le test avec une erreur
+  });
+
+  it('Adding a new record', async () => {
+    const student = new Student({name: 'Juby', articles: []});
+    try{
+      await student.save();
+      const savedStudent = await Student.findOne({name: 'Juby'});
+      savedStudent.articles.push({title: 'MongoDB'});
+      await savedStudent.save();
+      const updatedStudent = await Student.findOne({name: 'Juby'});
+      assert(updatedStudent.name === 'Juby');
+      assert(updatedStudent.articles[0].title === 'MongoDB');
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  });
+
+  it('Removes an article', async () => {
+    const student = new Student({name: 'Juby', articles: []});
+    try{
+      await student.save();
+      const savedStudent = await Student.findOne({name: 'Juby'});
+      savedStudent.articles.push({title: 'MongoDB'});
+      await savedStudent.save();
+      const updatedStudent = await Student.findOne({name: 'Juby'});
+      assert(updatedStudent.name === 'Juby');
+      assert(updatedStudent.articles[0].title === 'MongoDB');
+      updatedStudent.articles.pop();
+      await updatedStudent.save();
+      const studentWithoutArticle = await Student.findOne({name: 'Juby'});
+      assert(studentWithoutArticle.articles.length === 0);
+    } catch (error) {
+      console.log("Error:", error);
+    }
   });
 });

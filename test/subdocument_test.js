0a865a68a484885122a1e0046937a7483a3082c4
Pierre Lasante <pierre.lasante@videotron.ca>
2025-01-03 09:12:05 -0500

    testing a subdocument_test.js avec callbacks et async/await

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const Student = require('../src/student');
 
-describe('Subdocument', () => {
+describe('Subdocument sans callback', () => {
   it('Creates a subdocument', async () => {
     const student = new Student({
       name: 'Pierre',
@@ -16,5 +16,26 @@ describe('Subdocument', () => {
     } catch (error) {
       console.log("Error:", error);
     }
+  });
+});
+
+describe('Subdocument avec callback', () => {
+  it('Creates a subdocument', (done) => {
+    const student = new Student({
+      name: 'Pierre',
+      articles : [{title: 'JavaScript'}]
+    });
+
+    student.save()
+      .then(() => {
+        Student.findOne({ name: 'Pierre' })
+          .then((savedStudent) => {
+            assert(savedStudent.name === 'Pierre');
+            assert(savedStudent.articles[0].title === 'JavaScript');
+            done();
+          })
+          .catch(done); // Si une erreur se produit lors de la recherche, cela terminera le test avec une erreur
+      })
+      .catch(done); // Si une erreur se produit lors de l'enregistrement, cela terminera le test avec une erreur
   });
 });
